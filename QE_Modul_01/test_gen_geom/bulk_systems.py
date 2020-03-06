35a036003b89dbecc036b1ed9f92cac307ea36e3
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2020-03-06 15:03:57 +0700

    Test bcc

--- QE_Modul_01/test_gen_geom/bulk_systems.py
+++ QE_Modul_01/test_gen_geom/bulk_systems.py
@@ -14,3 +14,13 @@ def build_bulk_fcc_cubic(name, a):
                                         (0.5, 0, 0.5), (0.5, 0.5, 0)])
     return atoms
 
+def build_bulk_bcc(name, a):
+    b = a / 2
+    atoms = Atoms(name, cell=[(-b, b, b), (b, -b, b), (b, b, -b)], pbc=True)
+    return atoms
+
+
+def build_bulk_bcc_cubic(name, a):
+    atoms = Atoms(2*name, cell=(a, a, a), pbc=True,
+                        scaled_positions=[(0, 0, 0), (0.5, 0.5, 0.5)])
+    return atoms
--- QE_Modul_01/test_gen_geom/test_bulk_systems.py
+++ QE_Modul_01/test_gen_geom/test_bulk_systems.py
@@ -6,3 +6,8 @@ atoms.write("GEOM_H_fcc.xsf")
 atoms = build_bulk_fcc_cubic("H", 1.5)
 atoms.write("GEOM_H_fcc_cubic.xsf")
 
+atoms = build_bulk_bcc("H", 1.5)
+atoms.write("GEOM_H_bcc.xsf")
+
+atoms = build_bulk_bcc_cubic("H", 1.5)
+atoms.write("GEOM_H_bcc_cubic.xsf")
